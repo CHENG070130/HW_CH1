5c34edef8155d80d4c122c50ddc2dcab155c4343
CHENG070130 <jeff.you960130@gmail.com>
2025-10-20 00:03:35 +0800

    Add files via upload

---
+++ CH1 2.25/source/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//2.25
+int main(void) 
+{
+    printf("DDDDDDDDD\n");
+    printf("D       D\n");
+    printf("D       D\n");
+    printf(" D     D \n");
+    printf("  DDDDD  \n\n");
+
+    printf("CCCCCCCCC\n");
+    printf("C       C\n");
+    printf("C       C\n");
+    printf("C       C\n");
+    printf("C       C\n\n");
+ 
+    printf("        Y\n");
+    printf("       Y \n");
+    printf("YYYYYYY  \n");
+    printf("       Y \n");
+    printf("        Y\n\n");
+
+    return 0;
+}
